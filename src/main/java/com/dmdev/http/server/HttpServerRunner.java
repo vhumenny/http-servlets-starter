156d37371abc41bc2e31ae50465b342dc46ceae7
vhumenny <v.humenny@gmail.com>
2023-03-11 19:32:51 +0200

    json file type single threaded server and thread pooled server

---
+++ src/main/java/com/dmdev/http/server/HttpServerRunner.java
@@ -0,0 +1,8 @@
+package com.dmdev.http.server;
+
+public class HttpServerRunner {
+    public static void main(String[] args) {
+        var httpServer = new HttpServer(9000, 100);
+        httpServer.run();
+    }
+}
