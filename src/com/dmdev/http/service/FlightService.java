05204f0ed099061e30dfccc8b1db9d046f9d2e24
vhumenny <v.humenny@gmail.com>
2023-03-16 19:13:48 +0200

    practice

---
+++ src/com/dmdev/http/service/FlightService.java
@@ -0,0 +1,32 @@
+package com.dmdev.http.service;
+
+import com.dmdev.http.dao.FlightDao;
+import com.dmdev.http.dto.FlightDto;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.*;
+
+public class FlightService {
+    private static final FlightService INSTANCE = new FlightService();
+    private final FlightDao flightDao = FlightDao.getInstance();
+
+    private FlightService() {
+    }
+
+    public List<FlightDto> findAll() {
+        return flightDao.findAll().stream()
+                .map(flight -> new FlightDto(
+                        flight.getId(),
+                        """
+                                    %s - %s - %s
+                                """.formatted(flight.getDepartureAirportCode(), flight.getArrivalAirportCode(),
+                                flight.getStatus())
+                ))
+                .collect(toList());
+    }
+
+    public static FlightService getInstance() {
+        return INSTANCE;
+    }
+}
